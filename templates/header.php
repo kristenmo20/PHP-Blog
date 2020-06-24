f18240ecdbf3375ce12b1107520e0f5714b57e45
kristenmo20 <kristen.moss96@gmail.com>
2020-06-24 14:51:53 +0000

    Added assets from html5up templates

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Assignment 1</title>
+
+    <link rel="stylesheet" href="public/vendor/assets/css/main.css" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
