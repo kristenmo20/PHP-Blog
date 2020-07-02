c41a8134c30898401b40f79f644cc96777cad22f
kristenmo20 <kristen.moss96@gmail.com>
2020-07-02 19:33:24 +0000

    home page changes

--- private/app/views/main/index.php
+++ private/app/views/main/index.php
@@ -1,14 +1,14 @@
 <!-- Banner -->
-								<section id="banner">
-									<div class="content">
+								<section>
+									<div class="content" style="text-align: center;">
 										<header>
 											<h1>Assignment 1<br />
 											by Kristen Moss</h1>
 											<p>Editorial template by HTML5 UP</p>
 										</header>
 										<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
-										<ul class="actions">
-											<li><a href="#" class="button big">Learn More</a></li>
+										<ul class="actions align-center">
+											<li><a href="#" class="button big align-center">Learn More</a></li>
 										</ul>
 									</div>
 									<span class="image object">
@@ -18,16 +18,18 @@
 
                             <!-- Section -->
 								<section>
+                                <div class="align-center">
 									<header class="major">
 										<h2>Morgan Freeman</h2>
 									</header>
-									<div class="posts">
+									<div class="content align-center">
 										<article>
-											<a href="#" class="image"><img src="/public/images/morganfreeman.jpg" alt="" /></a>
-											<h3>Entry 1</h3>
+											<h3>Biography</h3>
 											<p>With an authoritative voice and calm demeanor, this ever popular American actor has grown into one of the most respected figures in modern US cinema. Morgan was born on June 1, 1937 in Memphis, Tennessee, to Mayme Edna (Revere), a teacher, and Morgan Porterfield Freeman, a barber. The young Freeman attended Los Angeles City College before serving several years in the US Air Force as a mechanic between 1955 and 1959. His first dramatic arts exposure was on the stage including appearing in an all-African American production of the exuberant musical Hello, Dolly!.</p>
                                             <p>Throughout the 1970s, he continued his work on stage, winning Drama Desk and Clarence Derwent Awards and receiving a Tony Award nomination for his performance in The Mighty Gents in 1978. In 1980, he won two Obie Awards, for his portrayal of Shakespearean anti-hero Coriolanus at the New York Shakespeare Festival and for his work in Mother Courage and Her Children. Freeman won another Obie in 1984 for his performance as The Messenger in the acclaimed Brooklyn Academy of Music production of Lee Breuer's The Gospel at Colonus and, in 1985, won the Drama-Logue Award for the same role. In 1987, Freeman created the role of Hoke Coleburn in Alfred Uhry's Pulitzer Prize-winning play Driving Miss Daisy, which brought him his fourth Obie Award. In 1990, Freeman starred as Petruchio in the New York Shakespeare Festival's The Taming of the Shrew, opposite Tracey Ullman. Returning to the Broadway stage in 2008, Freeman starred with Frances McDormand and Peter Gallagher in Clifford Odets' drama The Country Girl, directed by Mike Nichols.</p>
 										</article>
+									</div>
+                                </div>
                                 </section>
 
 							<!-- Section -->
