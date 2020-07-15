c4c82698c5484444d97844ccaf43afe95380f4ce
kristenmo20 <kristen.moss96@gmail.com>
2020-07-15 17:44:39 +0000

    bug fixes on blog pages

--- private/app/views/blog/item/index.php
+++ private/app/views/blog/item/index.php
@@ -6,14 +6,18 @@
 									</header>
 
 									<h2>Entry 1</h2>
-                                    <p>Blog Author: Kristen Moss</P>
-                                    <time datetime="06/24/2020">06/24/20 @ 7:25 pm</time>
+                                    <p>Blog Author: Kristen Moss</p>
+                                    <time>06/24/20 @ 7:25 pm</time><br>
+                                    
+                                    <br>
 									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
 									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
 									<p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
-                                    <p>Nullam rhoncus magna in congue blandit. Vestibulum consectetur volutpat imperdiet. Etiam bibendum vulputate elit non dapibus. Quisque enim est, venenatis non sollicitudin sed, viverra id elit. Integer vulputate, urna sed commodo tincidunt, leo felis iaculis lacus, et faucibus sem neque eu odio. Fusce mollis mi in est pharetra, id sagittis nunc mollis. Praesent egestas, sapien sit amet mattis ullamcorper, mi libero condimentum justo, vitae lacinia nibh nulla ac sapien. Vivamus laoreet lectus ac ex condimentum tristique. Phasellus tortor turpis, feugiat nec placerat id, congue dapibus turpis. Vestibulum iaculis rutrum purus et convallis. Nullam cursus, neque ut luctus finibus, felis neque vehicula dui, in mollis nunc elit vel augue. Vivamus nec feugiat quam, eu efficitur metus.</p>
+                                    <p>Nullam rhoncus magna in congue blandit. Vestibulum consectetur volutpat imperdiet. Etiam bibendum vulputate elit non dapibus. Quisque enim est, venenatis non sollicitudin sed, viverra id elit. Integer vulputate, urna sed commodo tincidunt, leo felis iaculis lacus, et faucibus sem neque eu odio. Fusce mollis mi in est pharetra, id sagittis nunc mollis. Praesent egestas, sapien sit amet mattis ullamcorper, mi libero condimentum justo, vitae lacinia nibh nulla ac sapien. Vivamus laoreet lectus ac ex condimentum tristique. Phasellus tortor turpis, feugiat nec placerat id, congue dapibus turpis. Vestibulum iaculis rutrum purus et convallis. Nullam cursus, neque ut luctus finibus, felis neque vehicula dui, in mollis nunc elit vel augue. Vivamus nec feugiat quam, eu efficitur metus.
+                                    </p>
                                     </article>
-                                    
 									<hr class="major" />
 								</section>
+                            </div>
+					</div>
                                     
