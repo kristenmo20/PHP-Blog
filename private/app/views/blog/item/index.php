c85a7bc683d60c5f8f372ed388631751380d24c9
kristenmo20 <kristen.moss96@gmail.com>
2020-07-24 05:56:12 +0000

    changed author in blog article

--- private/app/views/blog/item/index.php
+++ private/app/views/blog/item/index.php
@@ -6,7 +6,7 @@
 									</header>
 
 									<h2><?php echo($title) ?></h2>
-                                    <p>Blog Author: <?php echo($first_name . " " . $last_name) ?></p>
+                                    <p>Author: <?php echo($first_name . " " . $last_name) ?></p>
                                     <time><?php echo($publication_date) ?></time><br>
                                     
                                     <br>
