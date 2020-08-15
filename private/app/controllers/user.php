a7bb5aa03f024e7abfa29ec07bedb55b7ccb9291
kristenmo20 <kristen.moss96@gmail.com>
2020-08-15 17:02:37 +0000

    csrf tweaks

--- private/app/controllers/user.php
+++ private/app/controllers/user.php
@@ -6,77 +6,7 @@ class User extends Controller {
         parent::__construct();
     }
 
-    // function Index () {
-        
-    //     $loggedIn = $_SESSION["isLoggedIn"];
-        
-    //     if ($_SERVER["REQUEST_METHOD"] == "GET" && $loggedIn) {
-    //         $this->view("template/header");
-    //         $this->view("user/loggedIn");
-    //         $this->view("template/menu");
-    //         $this->view("template/footer");
-            
-    //     } else if ($_SERVER["REQUEST_METHOD"] == "GET" && !$loggedIn) {
-    //         $this->view("template/bsHeader");
-    //         $this->view("user/signin");
-    //         $this->view("template/bsFooter");
-
-    //     } 
-        
-    // }
-
-    // function Index () {
-    //     if ($_SERVER["REQUEST_METHOD"] == "POST" && (empty($_SESSION["isLoggedIn"]) || !$_SESSION["isLoggedIn"])) {
-    //         // $verify = password_hash("password", PASSWORD_DEFAULT);
-    //         $password = htmlentities($_POST["password"]);
-    //         $email = htmlentities($_POST["email"]);
-
-    //         // Query password based on email.
-                
-    //             $this->model("usermodel");
-    //             $auth = $this->usermodel->userAuth($email, $password);
-    //             echo($auth);
-
-    //             if ($auth) {
-    //                 $data = Array("header" => "Success");
-    //                 $this->view("template/header", $data);
-    //                 $this->view("user/success");
-    //                 $this->view("template/menu");
-    //                 $this->view("template/footer");
-                    
-    //                 $isVerified = $auth;
-    //                 $_SESSION["isLoggedIn"] = $isVerified;
-                    
-    //                 //header("Location: /");
-    //             } else {
-    //                 echo("Not Authenticated");
-    //                 echo("<br>");
-    //                 echo("Auth: " . $auth);
-    //                 echo("<br>");
-    //                 echo("Email: " . $email);
-    //                 echo("<br>");
-    //                 echo("Password: " . $password);
-    //                 echo("<br>");
-    //                 echo("isVerified: " . $isVerified);
-
-    //             }
-            
-    //     } else {
-    //         // display form
-    //         if (empty($_SESSION["isLoggedIn"]) || !$_SESSION["isLoggedIn"]) {
-    //                 $this->view("template/bsHeader");
-    //                 $this->view("user/signin");
-    //                 $this->view("template/bsFooter");
-    //         } else {
-    //             //return to homepage if get request and logged in
-    //             header("Location: /");
-    //         }
-            
-    //     }
-    // }
-
-    //no model version
-        function Index () {
+    function Index () {
         if ($_SERVER["REQUEST_METHOD"] == "POST" && (empty($_SESSION["isLoggedIn"]) || !$_SESSION["isLoggedIn"])) {
 
             $form_csrf = htmlentities($_POST["csrf"]);
@@ -84,7 +14,7 @@ class User extends Controller {
             $csrf = $_SESSION["csrf"];
 
             if ($csrf == $form_csrf && $csrf == $cookie_csrf) {
-                $verify = password_hash("iHeartClark", PASSWORD_DEFAULT);
+
                 $password = htmlentities($_POST["password"]);
                 $email = htmlentities($_POST["email"]);
 
@@ -94,15 +24,15 @@ class User extends Controller {
                     $auth = $this->usermodel->userAuth($email, $password);
                     echo($auth);
 
-                    if ($verify) {
+                    if ($auth) {
                         $data = Array("header" => "Success");
                         $this->view("template/header", $data);
                         $this->view("user/success");
                         $this->view("template/menu");
                         $this->view("template/footer");
                         
-                        $isVerified = $verify;
-                        $_SESSION["isLoggedIn"] = $verify;
+                        $isVerified = $auth;
+                        $_SESSION["isLoggedIn"] = $isVerified;
                         
                         //header("Location: /");
                     } else {
@@ -127,11 +57,11 @@ class User extends Controller {
                 // echo($form_csrf);
             }
             
+            
         } else {
             // display form
             if (empty($_SESSION["isLoggedIn"]) || !$_SESSION["isLoggedIn"]) {
                     $csrf = random_int(10000, 100000000);
-                    // echo($csrf);
                     $_SESSION['csrf'] = $csrf;
                     setcookie("csrf", $csrf);
                     
@@ -145,14 +75,83 @@ class User extends Controller {
         }
     }
 
+    //no model version
+    //     function Index () {
+    //     if ($_SERVER["REQUEST_METHOD"] == "POST" && (empty($_SESSION["isLoggedIn"]) || !$_SESSION["isLoggedIn"])) {
+
+    //         $form_csrf = htmlentities($_POST["csrf"]);
+    //         $cookie_csrf = htmlentities($_COOKIE["csrf"]);
+    //         $csrf = $_SESSION["csrf"];
+
+    //         if ($csrf == $form_csrf && $csrf == $cookie_csrf) {
+    //             $verify = password_hash("iHeartClark", PASSWORD_DEFAULT);
+    //             $password = htmlentities($_POST["password"]);
+    //             $email = htmlentities($_POST["email"]);
+
+    //             // Query password based on email.
+                    
+    //                 $this->model("usermodel");
+    //                 $auth = $this->usermodel->userAuth($email, $password);
+    //                 echo($auth);
+
+    //                 if ($verify) {
+    //                     $data = Array("header" => "Success");
+    //                     $this->view("template/header", $data);
+    //                     $this->view("user/success");
+    //                     $this->view("template/menu");
+    //                     $this->view("template/footer");
+                        
+    //                     $isVerified = $verify;
+    //                     $_SESSION["isLoggedIn"] = $verify;
+                        
+    //                     //header("Location: /");
+    //                 } else {
+    //                     echo("Not Authenticated");
+    //                     echo("<br>");
+    //                     echo("Auth: " . $auth);
+    //                     echo("<br>");
+    //                     echo("Email: " . $email);
+    //                     echo("<br>");
+    //                     echo("Password: " . $password);
+    //                     echo("<br>");
+    //                     echo("isVerified: " . $isVerified);
+
+    //                 }
+    //         } else {
+    //             echo("Bad csrf token.");
+    //             // echo("<br>");
+    //             // echo($csrf);
+    //             // echo("<br>");
+    //             // echo($cookie_csrf);
+    //             // echo("<br>");
+    //             // echo($form_csrf);
+    //         }
+            
+    //     } else {
+    //         // display form
+    //         if (empty($_SESSION["isLoggedIn"]) || !$_SESSION["isLoggedIn"]) {
+    //                 $csrf = random_int(10000, 100000000);
+    //                 // echo($csrf);
+    //                 $_SESSION['csrf'] = $csrf;
+    //                 setcookie("csrf", $csrf);
+                    
+    //                 $this->view("template/bsHeader");
+    //                 $this->view("user/signin", array("csrf" => $csrf));
+    //                 $this->view("template/bsFooter");
+    //         } else {
+    //             header("Location: /");
+    //         }
+            
+    //     }
+    // }
+
     function Logout () {
         $_SESSION["isLoggedIn"] = false;
         unset($_SESSION["isLoggedIn"]);
+        
         // Unset all of the session variables.
         $_SESSION = array();
 
-        // If it's desired to kill the session, also delete the session cookie.
-        // Note: This will destroy the session, and not just the session data!
         if (ini_get("session.use_cookies")) {
             $params = session_get_cookie_params();
             setcookie("PHPSESSID", '', time() - 42000,
